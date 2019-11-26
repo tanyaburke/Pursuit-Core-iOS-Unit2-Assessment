47a32d276c6eec94787f90b8cda78d65e1b6f8da
tanyaburke <tanyaburke@pursuit.org>
2019-11-25 20:59:57 -0500

    updated sliders

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/SettingsViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/SettingsViewController.swift
@@ -16,22 +16,103 @@ class SettingsViewController: UIViewController {
     
     @IBOutlet weak var greenSlider: UISlider!
     
-    @IBOutlet weak var blueSlidder: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
-    @IBOutlet weak var redValueNumber: UITextField!
+    @IBOutlet weak var redValue: UILabel!
     
-    @IBOutlet weak var greenValueNumber: UITextField!
-    @IBOutlet weak var blueValueNumber: UITextField!
+    @IBOutlet weak var greenValue: UILabel!
     
-  
+    @IBOutlet weak var blueValue: UILabel!
     
-  
+    @IBOutlet weak var alphaValue: UILabel!
+    
     @IBOutlet weak var alphaStepper: UIStepper!
+    
+    
+    var crayonColor: Crayon!
+    
+    var redV: CGFloat!
+    var greenV: CGFloat!
+    var blueV: CGFloat!
+    var alphaV: CGFloat!
+    var tempRedV: CGFloat!
+    var tempGreenV: CGFloat!
+    var tempBlueV: CGFloat!
+    var tempAlphaV: CGFloat!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        redV = CGFloat(crayonColor.red)
+        greenV = CGFloat(crayonColor.green)
+        blueV = CGFloat(crayonColor.blue)
+        alphaV = 1.0
+                tempRedV = redV
+                tempGreenV = greenV
+                tempBlueV = blueV
+        tempAlphaV = alphaV
+        
+        
+        view.backgroundColor = UIColor(red: CGFloat(redV)/CGFloat(255), green: CGFloat(greenV)/CGFloat(255), blue: CGFloat(blueV)/CGFloat(255), alpha: alphaV)
+        
+    }
+    func configureStepper() {
+       alphaStepper.minimumValue = 1.0
+       alphaStepper.maximumValue = 0.0
+       alphaStepper.stepValue = 0.1
+       
+       // default start value
+        alphaStepper.value = Double(alphaV)
+     }
+    func configureRedSlider() {
+      redSlider.minimumValue = 0
+      redSlider.maximumValue = 255
+//        redSlider.value = Double(redV)
+    }
+    func configureGreenSlider() {
+      greenSlider.minimumValue = 0
+      greenSlider.maximumValue = 255
+//      greenSlider.value = 2012
+    }
+    func configureBlueSlider() {
+  blueSlider.minimumValue = 0
+           blueSlider.maximumValue = 255
+      //      blueSlider.value = 2012
     }
     
-
-
+    
+    @IBAction func reset(_ sender: UIButton) {
+        view.alpha = alphaV
+        view.backgroundColor = UIColor(red: CGFloat(redV)/CGFloat(255), green: CGFloat(greenV)/CGFloat(255), blue: CGFloat(blueV)/CGFloat(255), alpha: alphaV)
+        
+    }
+    
+    @IBAction func redSlider(_ sender: UISlider) {
+        sender.value =
+            Float(tempRedV ?? redV )
+        tempRedV = CGFloat(sender.value)
+        redValue.text = tempRedV.description
+        view.backgroundColor = UIColor(red: CGFloat(tempRedV)/CGFloat(255), green: CGFloat(tempGreenV)/CGFloat(255), blue: CGFloat(tempBlueV)/CGFloat(255), alpha: alphaV)
+        
+    }
+    
+    @IBAction func greenSlider(_ sender: UISlider) {
+        
+        sender.value = Float(tempGreenV ?? greenV)
+        tempGreenV = CGFloat(sender.value)
+        greenValue.text = tempGreenV.description
+        view.backgroundColor = UIColor(red: CGFloat(tempRedV)/CGFloat(255), green: CGFloat(tempGreenV)/CGFloat(255), blue: CGFloat(tempBlueV)/CGFloat(255), alpha: alphaV)
+    }
+    
+    @IBAction func blueSlider(_ sender: UISlider) {
+        
+        sender.value = Float(tempBlueV ?? blueV)
+        tempBlueV = CGFloat(sender.value)
+        blueValue.text = tempBlueV.description
+        view.backgroundColor = UIColor(red: CGFloat(tempRedV)/CGFloat(255), green: CGFloat(tempGreenV)/CGFloat(255), blue: CGFloat(tempBlueV)/CGFloat(255), alpha: alphaV)
+    }
+    
 }
--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -9,28 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
+    var crayons = [Crayon](){
+        didSet{
+            tableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
-    super.viewDidLoad()
+        super.viewDidLoad()
         tableView.dataSource = self
-    
-  }
-
-
+        loadData()
+    }
+    func loadData(){
+        crayons = Crayon.allTheCrayons
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+         print("prepare for segue")
+        guard let settingsVC = segue.destination as? SettingsViewController,
+            let indexPath = tableView.indexPathForSelectedRow else {
+                fatalError("unable to connect to view controller")
+        }
+        let crayon = crayons[indexPath.row]
+        settingsVC.crayonColor = crayon
+    }
 }
 
+
+
 extension ViewController: UITableViewDataSource {
-   
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return crayons.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+        let color = crayons[indexPath.row]
+        
+        cell.textLabel?.text = color.name
+        cell.detailTextLabel?.text = color.hex
+        cell.alpha = 1
+        
+        if color.red == 0 && color.green == 0 && color.blue == 0 {
+            cell.textLabel?.textColor = .white
+            cell.detailTextLabel?.textColor = .white
+            cell.backgroundColor = UIColor(red: CGFloat(color.red)/CGFloat(255), green: CGFloat(color.green)/CGFloat(255), blue: CGFloat(color.blue)/CGFloat(255), alpha: 0.85)
+        } else {
+            cell.backgroundColor = UIColor(red: CGFloat(color.red)/CGFloat(255), green: CGFloat(color.green)/CGFloat(255), blue: CGFloat(color.blue)/CGFloat(255), alpha: 0.85)
+        }
+        return cell
     }
     
     
